daf417fd5653e7b5306b4b165e2df1f8923d9020
red-black-tree <rb-tree@foxmail.com>
2023-11-08 15:00:24 +0800

    Site updated: 2023-11-08 15:00:21

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log(`
+ CCCC M   M K   K        AAA  K   K       IIIII  OOO  IIIII !! !! !!
+C     MM MM K  K        A   A K  K          I   O   O   I   !! !! !!
+C     M M M KKK         AAAAA KKK           I   O   O   I   !! !! !!
+C     M   M K  K        A   A K  K          I   O   O   I
+ CCCC M   M K   K       A   A K   K       IIIII  OOO  IIIII !! !! !!`)
